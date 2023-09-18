d661bac23d9b9ddd04b2d1220e23f0ab6017a826
Jiawei Wu <robertwu1996@gmail.com>
2023-09-18 16:21:02 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+};
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  await newUser
+    .save()
+    .then(() => {
+      console.log("New user created.");
+      res.render("secrets");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/login", async (req, res) => {
+  const userName = req.body.username;
+  const password = req.body.password;
+
+  const foundUser = await User.findOne({ email: userName }).catch((err) => {
+    console.log(err);
+  });
+
+  if (foundUser) {
+    if (foundUser.password === password) {
+      console.log("Successfully login.");
+      res.render("secrets");
+    } else {
+      console.log("fail to login.");
+    }
+    }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
