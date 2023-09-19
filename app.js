9b4b2c144c5c6e5e6f35417feed119941345324c
Jiawei Wu <robertwu1996@gmail.com>
2023-09-18 22:01:54 -0400

    use bcrypt with 10 salt rounds

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import mongoose from "mongoose";
-import md5 from "md5";
+import bcrypt from "bcrypt";
+const saltRounds = 10;
 
 const app = express();
 
@@ -24,7 +25,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-
 const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
@@ -40,37 +40,38 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-
-  await newUser
-    .save()
-    .then(() => {
-      console.log("New user created.");
-      res.render("secrets");
-    })
-    .catch((err) => {
-      console.log(err);
+  bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
     });
+
+    await newUser
+      .save()
+      .then(() => {
+        console.log("New user created.");
+        res.render("secrets");
+      })
+      .catch(console.log(err));
+  });
 });
 
 app.post("/login", async (req, res) => {
   const userName = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   const foundUser = await User.findOne({ email: userName }).catch((err) => {
     console.log(err);
   });
-
   if (foundUser) {
-    if (foundUser.password === password) {
-      console.log("Successfully login.");
-      res.render("secrets");
-    } else {
-      console.log("fail to login.");
-    }
+    await bcrypt.compare(password, foundUser.password, (err, result) => {
+      if (result) {
+        console.log("Successfully login.");
+        res.render("secrets");
+      } else {
+        console.log("fail to login.");
+      }
+    });
   }
 });
 
